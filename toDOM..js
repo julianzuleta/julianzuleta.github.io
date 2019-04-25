b7900dc012e6a8b58d2ae6bd9747cb5e5d88d0d7
julianzuleta <43024722+julianzuleta@users.noreply.github.com>
2019-04-25 11:36:24 -0700

    Create toDOM..js

---
+++ toDOM..js
@@ -0,0 +1,36 @@
+const toDOM = props => {
+	let { parent, child } = props
+	let dataAttr = 'data-current-elem'
+	let parentElems = document.querySelectorAll(parent)
+	let targetElements = Object.keys(parentElems).map(elem => parentElems[elem])
+
+	targetElements.forEach(parentEl => {
+		if (child) {
+			child.forEach(childEl => {
+				let newElem = document.createElement(childEl.elem)
+				newElem.setAttribute(dataAttr, '')
+
+				if (childEl.text) {
+					newElem.innerHTML = childEl.text;
+				}
+
+				for (let attrKey in childEl.attrs) {
+					newElem.setAttribute(attrKey, childEl.attrs[attrKey])
+				}
+
+				parentEl.appendChild(newElem)
+				parentEl.removeAttribute(dataAttr)
+
+
+				if (childEl['child']) {
+					toDOM({
+						parent: `[${dataAttr}]`,
+						child: childEl['child']
+					})
+				} else {
+					newElem.removeAttribute(dataAttr)
+				}
+			})
+		}
+	})
+}
